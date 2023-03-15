52e08a1a8a4aed80454f65169fa87c5b334577be
jeromyzhang <zjdx1998@gmail.com>
2023-03-14 23:24:20 -0700

    Finish Lab of A7

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,12 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <div>
-      <Link to="/">Labs</Link> |
-      <Link to="/hello">Hello</Link> |
-      <Link to="/tuiter">Tuiter</Link>
+      <Link to="/">Labs</Link> |<Link to="/">Assignment 6</Link> |
+      <Link to="/a7">Assignment 7</Link> |<Link to="/tuiter">Tuiter</Link>
     </div>
-  )
+  );
 }
 
 export default Nav;
